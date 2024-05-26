00f5f86a5f9603ea4bf1c707c4666f14a96ffc8c
Dinesh <dineshgarig@gmail.com>
2024-05-26 11:55:12 +0530

    side button work effect done

--- scripts/Hero.js
+++ scripts/Hero.js
@@ -1,6 +1,8 @@
 let menu = document.querySelector("nav i")
 let cross = document.querySelector(".full i")
-
+let rightarrow = document.querySelector(".ri-arrow-right-line")
+let leftarrow = document.querySelector(".ri-arrow-left-line")
+console.log(rightarrow)
 var tl =  gsap.timeline({paused:true})
 tl.to(".full",{
     right:"0",
@@ -22,4 +24,50 @@ menu.addEventListener("click",function(){
 })
 cross.addEventListener("click",function(){
     tl.reverse()
+})
+rightarrow.addEventListener("click",function(){
+    gsap.to(".sidepage",{
+        right:"0",
+        duration:1
+
+    })
+    gsap.to(".left-shoe-1",{
+        x:300,
+        opacity:0,
+        duration:1
+    })
+    gsap.to(".left-shoe-2",{
+        y:300,
+        opacity:0,
+        duration:1
+    })
+    gsap.to(".right-shoe-1",{
+        y:300,
+        opacity:0,
+        duration:1,
+       
+    })
+    
+})
+leftarrow.addEventListener("click",function(){
+    gsap.to(".sidepage",{
+        right:"-110%",
+        duration:1,
+    })
+    gsap.to(".left-shoe-1",{
+        x:100,
+        opacity:1,
+        duration:1
+    })
+    gsap.to(".left-shoe-2",{
+        y:-50,
+        opacity:1,
+        duration:1
+    })
+    gsap.to(".right-shoe-1",{
+        y:-100,
+        opacity:1,
+        duration:0.5,
+        
+    })
 })
